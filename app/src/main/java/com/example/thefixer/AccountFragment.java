a513e4c213783664ab1c69a1a36a571e9f8b782d
vuthingngoc <63228228+vuthingngoc@users.noreply.github.com>
2021-09-28 09:07:58 +0700

    update fragment location

--- app/src/main/java/com/example/thefixer/AccountFragment.java
+++ app/src/main/java/com/example/thefixer/AccountFragment.java
@@ -1,27 +1,30 @@
-package com.example.thefixer.ui.home;
+package com.example.thefixer;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.example.thefixer.R;
-import com.example.thefixer.databinding.FragmentHomeBinding;
+import com.example.thefixer.databinding.FragmentAccountBinding;
 
-public class HomeFragment extends Fragment {
+public class AccountFragment extends Fragment {
 
-    public HomeFragment(){
+    public AccountFragment(){
 
     }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
         return view;
     }
 
--- app/src/main/java/com/example/thefixer/HomeFragment.java
+++ app/src/main/java/com/example/thefixer/HomeFragment.java
@@ -1,30 +1,23 @@
-package com.example.thefixer.ui.account;
+package com.example.thefixer;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.thefixer.R;
-import com.example.thefixer.databinding.FragmentAccountBinding;
+public class HomeFragment extends Fragment {
 
-public class AccountFragment extends Fragment {
-
-    public AccountFragment(){
+    public HomeFragment(){
 
     }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
         return view;
     }
 
